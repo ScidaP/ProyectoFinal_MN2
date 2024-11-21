5e7b0fae45050e018adef357a02694ed658fea12
Patricio Scidá <patricioscida@gmail.com>
2024-11-21 09:15:25 -0300

    ejecuciones cluster agregadas

---
+++ Producto Escalar 25200/producto_escalar.py
@@ -0,0 +1,52 @@
+from mpi4py import MPI
+import numpy as np
+import time
+
+# Definir el tamaño de los arreglos
+TAMANIO = 20000
+
+# Función para llenar un arreglo con valores
+def llenar_matriz(tamanio):
+    return np.arange(1, tamanio + 1)
+
+# Función para calcular el producto vectorial parcial
+def prod_vectorial(v1, v2, a, b):
+    suma = 0
+    for i in range(a, b):
+        suma += v1[i] * v2[i]
+    return suma
+
+def main():
+    # Inicializar MPI
+    comm = MPI.COMM_WORLD
+    ID_Proceso = comm.Get_rank()
+    TotalProcesos = comm.Get_size()
+
+    # Crear y llenar las matrices
+    arre1 = llenar_matriz(TAMANIO)
+    arre2 = llenar_matriz(TAMANIO)
+
+    # División del trabajo
+    cant_pasos = TAMANIO // TotalProcesos
+    nva_a = ID_Proceso * cant_pasos
+    nva_b = (ID_Proceso + 1) * cant_pasos
+
+    # Calcular el producto vectorial parcial
+    start_time = MPI.Wtime()
+    resultado_i = prod_vectorial(arre1, arre2, nva_a, nva_b)
+    print(f"Soy el proceso {ID_Proceso} y mi suma parcial es {resultado_i}")
+
+    # Reducir los resultados parciales al proceso 0
+    resultado_t = comm.reduce(resultado_i, op=MPI.SUM, root=0)
+
+    # Obtener el tiempo máximo
+    end_time = MPI.Wtime()
+    elapsed_time = end_time - start_time
+    max_time = comm.reduce(elapsed_time, op=MPI.MAX, root=0)
+
+    if ID_Proceso == 0:
+        print(f"\nEl resultado total es {resultado_t}")
+        print(f"Tiempo total de ejecución = {max_time} segundos")
+
+if __name__ == "__main__":
+    main()
